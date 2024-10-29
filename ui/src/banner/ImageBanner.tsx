ed375875e99972d1c4651f92e66f2d850a8b1349
UVMapping <107897863+KendallRey@users.noreply.github.com>
2024-10-29 21:15:45 +0800

    ui, update ImageBanner components

--- ui/src/banner/ImageBanner.tsx
+++ ui/src/banner/ImageBanner.tsx
@@ -1,22 +1,25 @@
-import { MuiBox } from '@nx-next-js-micro/components';
+import { ILayout, MuiBox, IMuiBox } from '@nx-next-js-micro/components';
 import React from 'react';
 
 type IImageBanner = {
   imageUrl: string;
-};
+} & IMuiBox;
 
 export const ImageBanner: React.FC<IImageBanner> = (props) => {
-  const { imageUrl } = props;
+  const { children, className, imageUrl, ...otherProps } = props;
   return (
     <MuiBox
-      className="h-full"
+      className={`${className || ''} h-screen`}
       style={{
         backgroundImage: `url(${imageUrl})`,
-        backgroundPosition: 'top center',
+        backgroundPosition: 'bottom center',
         backgroundRepeat: 'no-repeat',
         backgroundSize: 'cover',
         backgroundAttachment: 'fixed',
       }}
-    ></MuiBox>
+      {...otherProps}
+    >
+      {children}
+    </MuiBox>
   );
 };
