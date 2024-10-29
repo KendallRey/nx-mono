ef0e8ab1034b67feb4a6685ee8ce82c8c3824058
UVMapping <107897863+KendallRey@users.noreply.github.com>
2024-10-29 23:23:52 +0800

    portfolio, update initial page

--- apps/portfolio/src/app/page.tsx
+++ apps/portfolio/src/app/page.tsx
@@ -1,11 +1,16 @@
-import { MuiBox } from "@nx-next-js-micro/components";
+import { MuiBox, MuiStack, MuiTypography } from "@nx-next-js-micro/components";
 import { ImageBanner } from "@nx-next-js-micro/ui";
 
 export default function HomePage() {
 
   return (
     <MuiBox className="h-screen">
-      <ImageBanner imageUrl="/images/banner.png"/>
+      <ImageBanner imageUrl="/images/banner_2.png" className="grid items-center justify-center">
+        <MuiStack>
+          <MuiTypography color='white' fontSize={32}>Hello there,</MuiTypography>
+          <MuiTypography color='white' fontSize={42}>KR here.</MuiTypography>
+        </MuiStack>
+      </ImageBanner>
       <MuiBox>1</MuiBox>
       <MuiBox>2</MuiBox>
       <MuiBox>3</MuiBox>
