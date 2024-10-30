158264a6f517f8d633758e3c9c9488cc25367e4e
UVMapping <107897863+KendallRey@users.noreply.github.com>
2024-10-31 00:04:54 +0800

    portfolio, add sample markdown

--- apps/portfolio/src/app/page.tsx
+++ apps/portfolio/src/app/page.tsx
@@ -1,7 +1,12 @@
-import { MuiBox, MuiStack, MuiTypography } from "@nx-next-js-micro/components";
+'use client';
+
+import { RMarkdownEditor, MuiBox, MuiStack, MuiTypography } from "@nx-next-js-micro/components";
 import { ImageBanner } from "@nx-next-js-micro/ui";
+import { useState } from "react";
 
 export default function HomePage() {
+
+  const [value, setValue] = useState<string | undefined>('')
 
   return (
     <MuiBox className="h-screen">
@@ -11,6 +16,7 @@ export default function HomePage() {
           <MuiTypography color='white' fontSize={42}>KR here.</MuiTypography>
         </MuiStack>
       </ImageBanner>
+      <RMarkdownEditor value={value} onChange={setValue} visible  height="500px"/>
       <MuiBox>1</MuiBox>
       <MuiBox>2</MuiBox>
       <MuiBox>3</MuiBox>
