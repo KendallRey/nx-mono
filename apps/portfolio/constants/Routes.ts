7d13dbe716ef278322e37ef5768d61f90d4da804
UVMapping <107897863+KendallRey@users.noreply.github.com>
2024-11-13 22:14:47 +0800

    portfolio, change ROUTES, projects to tech-stack

--- apps/portfolio/constants/Routes.ts
+++ apps/portfolio/constants/Routes.ts
@@ -2,6 +2,6 @@ import { IRoute } from '@nx-next-js-micro/components';
 
 export const ROUTES: IRoute[] = [
   { href: '/', name: 'Home' },
-  { href: '/project', name: 'Projects' },
+  { href: '/tech-stack', name: 'Tech Stack' },
   { href: '/contact', name: 'Contact' },
 ] as const;
